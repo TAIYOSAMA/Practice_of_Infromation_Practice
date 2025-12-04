79d7807283bf3042c86fff926265b608f584cb71
TAIYOSAMA <taiyo180601@gmail.com>
2025-12-04 15:49:16 +0900

    add: q09a.cを追加しました

---
+++ src/q09a.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define MAX_LEN 100
+
+int main () {
+    char str[MAX_LEN];
+
+    // 文字列を入力
+    printf("Input:");
+    scanf("%s", str);
+
+    // 大文字に変換
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] >= 'a' && str[i] <= 'z')
+            str[i] = str[i] - ('a' - 'A');
+    }
+
+    // 出力
+    printf("Output:%s\n", str);
+}
