096d9b7f41c5e259feb885448eb7bc2f5aeb25db
TAIYOSAMA <taiyo180601@gmail.com>
2026-01-15 14:26:02 +0900

    ref: 素数判定関数のカッコを削除しました。

--- src/q04b.c
+++ src/q04b.c
@@ -3,11 +3,8 @@
 // xが素数かどうかを判定
 int isPrime (int x) {
     if (x == 1) return 0;
-    for (int i = 2; i * i <= x; i++) {
-        if (x % i == 0) {
-            return 0;
-        }
-    }
+    for (int i = 2; i * i <= x; i++) 
+        if (x % i == 0) return 0;
     return 1;
 }
 
